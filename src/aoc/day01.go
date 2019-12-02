da5c3630eab8275b41e9c96dba941c941dd039ec
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2019-12-02 00:13:46 +0000

    Go uses tabs by default

--- src/aoc/day01.go
+++ src/aoc/day01.go
@@ -1,50 +1,50 @@
 package main
- 
+
 import (
-        "bufio"
-        "fmt"
-        "log"
-        "os"
-        "strconv"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
- 
+
 func main() {
-        file, err := os.Open("../data/day01.txt")
+	file, err := os.Open("../data/day01.txt")
 
-        if err != nil {
-                log.Fatalf("failed opening file: %s", err)
-        }
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
 
-        scanner := bufio.NewScanner(file)
-        scanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-        var part1 = 0
-        var part2 = 0
+	var part1 = 0
+	var part2 = 0
 
-        for scanner.Scan() {
-                i, err := strconv.Atoi(scanner.Text())
-                if err != nil {
-                        // handle error
-                        fmt.Println(err)
-                        os.Exit(2)
-                }
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+			os.Exit(2)
+		}
 
-                var moduleFuel = int(float64(i/3) - 2)
-                part1 += moduleFuel
-                part2 += moduleFuel
+		var moduleFuel = int(float64(i/3) - 2)
+		part1 += moduleFuel
+		part2 += moduleFuel
 
-                for {
-                        moduleFuel = int(float64(moduleFuel/3) - 2)
-                        if moduleFuel <= 0 {
-                                break
-                        }
+		for {
+			moduleFuel = int(float64(moduleFuel/3) - 2)
+			if moduleFuel <= 0 {
+				break
+			}
 
-                        part2 += moduleFuel
-                }
-        }
+			part2 += moduleFuel
+		}
+	}
 
-        file.Close()
+	file.Close()
 
-        fmt.Println("Part1:", part1)
-        fmt.Println("Part2:", part2)
+	fmt.Println("Part1:", part1)
+	fmt.Println("Part2:", part2)
 }
